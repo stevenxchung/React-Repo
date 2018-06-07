050eca76473e7f03309d323725b8225d82cd36e6
Steven <stevenxchung@gmail.com>
2018-06-06 22:04:43 -0400

    Add Product component to app.js

--- Coursework/Fullstack React/Part I/01 - Project Hunt/voting_app/public/js/app.js
+++ Coursework/Fullstack React/Part I/01 - Project Hunt/voting_app/public/js/app.js
@@ -11,8 +11,23 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className="item">
+        <div className='image'>
+          <img src="images/products/image-aqua.png" />
+        </div>
+        <div className="middle aligned content">
+          <div className="description">
+            <a>Fort Knight</a>
+            <p>Authentic reaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img
+              className="ui avatar image"
+              src="images/avatars/daniel.jpg"
+            />
+          </div>
+        </div>
       </div>
     );
   }
@@ -22,4 +37,3 @@ ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
 );
-
