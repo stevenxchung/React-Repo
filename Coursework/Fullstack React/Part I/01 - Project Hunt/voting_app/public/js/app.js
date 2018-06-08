e9b1507a1b64db5f9b0bd1df8a48b4d4479fc0ac
Steven <stevenxchung@gmail.com>
2018-06-08 19:41:53 -0400

    Vote button working

--- Coursework/Fullstack React/Part I/01 - Project Hunt/voting_app/public/js/app.js
+++ Coursework/Fullstack React/Part I/01 - Project Hunt/voting_app/public/js/app.js
@@ -1,7 +1,38 @@
-
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
   render() {
-    const productComponents = Seed.products.map((product) => (
+    const products = this.state.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = this.state.products.map((product) => (
       <Product
         key={"product-" + product.id}
         id={product.id}
@@ -11,6 +42,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
     return (
@@ -22,6 +54,17 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props); // Always call this when working with a custom component
+
+    // Custom method bindings here
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
@@ -29,12 +72,12 @@ class Product extends React.Component {
           <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
-        <div className="header">
-          <a>
-            <i className="large caret up icon" />
-          </a>
-          {this.props.votes}
-        </div>
+          <div className="header">
+            <a onClick={this.handleUpVote}>
+              <i className="large caret up icon" />
+            </a>
+            {this.props.votes}
+          </div>
           <div className='description'>
             <a>
             {this.props.title}
