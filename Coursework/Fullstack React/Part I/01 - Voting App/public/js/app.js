158d16b8235763b5718e6ac1bfb1444134013ff6
Steven <stevenxchung@gmail.com>
2018-06-09 13:50:33 -0400

    Add commentary to app.js

--- Coursework/Fullstack React/Part I/01 - Voting App/public/js/app.js
+++ Coursework/Fullstack React/Part I/01 - Voting App/public/js/app.js
@@ -1,12 +1,15 @@
 class ProductList extends React.Component {
+  // Initialize state to an empty array
   state = {
     products: [],
   };
 
+  // Lifecycle method which is invoked after component has mounted to the page
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
+  // Adds one vote to the item that was clicked on
   handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
@@ -23,12 +26,15 @@ class ProductList extends React.Component {
   }
 
   render() {
+    // Sort votes from largest to smallest
     const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
+    // Create an array of components using map
     const productComponents = this.state.products.map((product) => (
+      // Passing down props from parent component to child
       <Product
-        key={"product-" + product.id}
+        key={'product-' + product.id}
         id={product.id}
         title={product.title}
         description={product.description}
@@ -39,8 +45,9 @@ class ProductList extends React.Component {
         onVote={this.handleProductUpVote}
       />
     ));
+    // Return the new array of components stored in productComponents
     return (
-      <div className="ui unstackable items">
+      <div className='ui unstackable items'>
         {productComponents}
       </div>
     );
@@ -54,14 +61,15 @@ class Product extends React.Component {
 
   render() {
     return (
+      // Use props (this.props) that have been passed down from the parent component
       <div className='item'>
         <div className='image'>
           <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
-          <div className="header">
+          <div className='header'>
             <a onClick={this.handleUpVote}>
-              <i className="large caret up icon" />
+              <i className='large caret up icon' />
             </a>
             {this.props.votes}
           </div>
@@ -86,6 +94,7 @@ class Product extends React.Component {
   }
 }
 
+// Instruct React to render ProductList inside an HTML element with the id of 'content'
 ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
